a9be215b113cef5f7c702c515993d34b54d9a84f
Joao Otavio <joaolima.felipelli@gmail.com>
2022-01-15 10:14:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/zima/Zima/ZimaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zima.Zima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zima/Zima/ZimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zima.Zima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
